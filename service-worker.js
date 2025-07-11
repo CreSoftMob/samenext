e6b4a67f2de171e796db344c6e3fb194ba7661c4
otto monteiro <afonsodsilvamendes@icloud.com>
2025-07-10 21:27:29 -0300

    my see about life is

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+// service-worker.js
+
+const CACHE_NAME = 'samenext-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './2.jpeg'
+];
+
+// Instalação
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Ativação
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
